41a88afc4a5caba09816112572ea9ee305b31a09
harunbekcan <harunbekcan@gmail.com>
2024-11-25 23:17:57 +0300

    PullToRefresh has been added to the HomeScreen.

--- androidApp/src/main/java/com/harunbekcan/kmpmoviedbapp/android/home/HomeScreen.kt
+++ androidApp/src/main/java/com/harunbekcan/kmpmoviedbapp/android/home/HomeScreen.kt
@@ -13,7 +13,10 @@ import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
@@ -22,50 +25,60 @@ import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.unit.dp
 import com.harunbekcan.kmpmoviedbapp.domain.model.Movie
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
     uiState: HomeScreenState,
     loadNextMovies: (Boolean) -> Unit,
-    navigateToDetail: (Movie) -> Unit
+    navigateToDetail: (Movie) -> Unit,
 ) {
+
+    val swipeRefreshState = rememberPullToRefreshState()
 
     Box(
         modifier = modifier
             .fillMaxSize()
             .background(color = MaterialTheme.colorScheme.background)
     ) {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            contentPadding = PaddingValues(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ){
-            itemsIndexed(
-                uiState.movies,
-                key = { index, movie ->
-                    "${movie.id}-$index"
-                }
-            ) { index, movie ->
-                MovieListItem(movie = movie, onMovieClick = { navigateToDetail(movie) })
+        PullToRefreshBox(
+            modifier = modifier.align(Alignment.TopCenter),
+            state = swipeRefreshState,
+            isRefreshing = uiState.refreshing,
+            onRefresh = { loadNextMovies(true) }) {
 
-                if (index >= uiState.movies.size - 1 && !uiState.loading && !uiState.loadFinished) {
-                    LaunchedEffect(key1 = Unit) { loadNextMovies(false) }
-                }
-            }
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                contentPadding = PaddingValues(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                itemsIndexed(
+                    uiState.movies,
+                    key = { index, movie ->
+                        "${movie.id}-$index"
+                    }
+                ) { index, movie ->
+                    MovieListItem(movie = movie, onMovieClick = { navigateToDetail(movie) })
 
-            if (uiState.loading && uiState.movies.isNotEmpty()){
-                item(span = { GridItemSpan(2) }){
-                    Row(
-                        modifier = modifier
-                            .fillMaxWidth()
-                            .padding(16.dp),
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        CircularProgressIndicator(
-                            color = Red
-                        )
+                    if (index >= uiState.movies.size - 1 && !uiState.loading && !uiState.loadFinished) {
+                        LaunchedEffect(key1 = Unit) { loadNextMovies(false) }
+                    }
+                }
+
+                if (uiState.loading && uiState.movies.isNotEmpty()) {
+                    item(span = { GridItemSpan(2) }) {
+                        Row(
+                            modifier = modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            CircularProgressIndicator(
+                                color = Red
+                            )
+                        }
                     }
                 }
             }
